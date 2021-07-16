1a7a8d7c81db3954335155b0699c9002eb79cb5e
Danielle Hancock <daniellenicolehancock@gmail.com>
2021-07-16 18:05:42 -0400

    upload executables

---
+++ Debug/scriptFile.js
@@ -0,0 +1,8 @@
+function random(obj) {
+	var x = Math.floor(Math.random() * Math.floor(700) + 50);
+	var y = Math.floor(Math.random() * Math.floor(500) + 50);
+	obj.setPos(x,y);
+	return "Script Success"
+}
+
+
